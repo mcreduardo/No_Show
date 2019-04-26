ea4917a57f32e9f3420954f1f7a41ba8073fb4fb
Eduardo Rocha <edu181294@gmail.com>
2019-04-26 14:17:32 -0500

    modilarized implementation

--- Code/NN_sigmoid.py
+++ Code/NN_sigmoid.py
@@ -1,185 +1,269 @@
-# Hospital appointment no-show prediction
 # Simple NN using TensorFlow
 # April 2019
 # Eduardo Moura Cirilo Rocha
 
 import tensorflow as tf
-import pandas as pd # load csv data
+#import pandas as pd # load csv data
 import numpy as np
-import matplotlib.pyplot as plt # plotting
+#import matplotlib.pyplot as plt # plotting
 
-from sklearn.model_selection import train_test_split # split into training and test set
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_recall_curve
-from sklearn.utils.fixes import signature
-from sklearn.metrics import average_precision_score
+#from sklearn.metrics import confusion_matrix
+#from sklearn.metrics import precision_recall_curve
+#from sklearn.utils.fixes import signature
+#from sklearn.metrics import average_precision_score
 
 import dataset as ds
 
 
-# Load data #######################################################################################
+class NN_Sigmoid:
+    """
+    Simple NN for binary classification using Sigmoid function.
+    Fully connected layers with ReLU activation.
+    """
 
-noShow = ds.import_data_df([ds._FILE_PATHS['merged']])
-noShow_X, noShow_y = noShow.iloc[:, :-1].values, noShow.iloc[:, -1].values
-noShow_y = np.array([[i] for i in noShow_y])
-#noShow_y= pd.get_dummies(noShow_y).values
+    ####################################
+    def __init__(
+        self, hiddenLayers, numFeatures, numLabels, learning_rate, 
+        cross_entropy_weight = 1,
+        optimizer = "GD"
+    ):
+        """
+        Constructor: Build network
 
+        "hiddenLayers" contains the structure of the network to be trained:
+            list with number of neurons per hidden layer.
 
-trainX, testX, trainY, testY = train_test_split(
-    noShow_X, noShow_y, test_size=0.33, random_state=42,
-    stratify = noShow_y
-)
+        "numFeatures" is the number of features
 
+        "numLabels" is the number of labels
 
+        "learning_rate" is the learning rate used for training
 
+        "cross_entropy_weight" tf.nn.weighted_cross_entropy_with_logits if != 1
 
-# Training parameters #############################################################################
+        "optimizer" method used to minimize cost. Values: "GD" (gradient descent, default) or "Adam"
+        """
 
-# Number of Epochs in our training
-numEpochs = 10000
+        # tf session
+        self._sess = tf.Session()
 
-# Hidden layers
-hiddenLayers = [50, 50, 30, 10] 
-numHiddenLayers = len(hiddenLayers)
+        self._numHiddenLayers = len(hiddenLayers)
 
-# Defining our learning rate iterations (decay)
-'''
-learningRate = tf.train.exponential_decay(learning_rate=0.0008,
-                                          global_step= 1,
-                                          decay_steps=trainX.shape[0],
-                                          decay_rate= 0.95,
-                                          staircase=True)
-'''
-learning_rate = 0.01 # AdamOptimizer below
+        # Placeholders
+        self._X = tf.placeholder(tf.float32, [None, numFeatures])
+        self._yGold = tf.placeholder(tf.float32, [None, numLabels])
 
-# Output
-printOutput = True
-plotLoss = True
-printConfusionMatrix = True
-test_set_result_path = 'Results/test_set_result.csv'
-plotPRCurve = True
+        # weights init
+        self._initializer = tf.contrib.layers.xavier_initializer()
 
-###################################################################################################
+        self._layers = []
+        # input layer
+        self._input_layer = self._X
+        self._layers.append(self._input_layer)
+        # hidden layers (ReLU activation)
+        for i in range(self._numHiddenLayers):
+            self._h = tf.layers.dense(self._layers[-1], hiddenLayers[i], activation=tf.nn.relu,
+                kernel_initializer=self._initializer)
+            self._layers.append(self._h)
+        # output layer
+        self._output_layer = tf.layers.dense(self._layers[-1], numLabels, activation=None)
+        self._predicted = tf.nn.sigmoid(self._output_layer)
+        self._correct_pred = tf.equal(tf.round(self._predicted), self._yGold)
+        self._accuracy = tf.reduce_mean(tf.cast(self._correct_pred, tf.float32))
+        # loss
+        if cross_entropy_weight == 1:
+            self._cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=self._yGold, logits=output_layer)
+        else:
+            self._cross_entropy = tf.nn.weighted_cross_entropy_with_logits(
+                pos_weight = cross_entropy_weight,targets=self._yGold, logits=self._output_layer)
+        self._cost = tf.reduce_mean(self._cross_entropy)
+        if optimizer == "Adam":
+            self._optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self._cost)
+        if optimizer == "GD":
+            self._optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(self._cost)
+        # --->>> implement optimizer != GD or Adam handler
 
-# number of features
-numFeatures = trainX.shape[1]
+        # saver to save session
+        self._saver = tf.train.Saver()
 
-# number of classes
-numLabels = trainY.shape[1]
-
-# Placeholders
-# 'None' means TensorFlow shouldn't expect a fixed number in that dimension
-X = tf.placeholder(tf.float32, [None, numFeatures])
-yGold = tf.placeholder(tf.float32, [None, numLabels])
-
-is_training=tf.Variable(True,dtype=tf.bool)
-
-# weights init
-initializer = tf.contrib.layers.xavier_initializer()
-
-
-###################################################################################################
-# Model
-
-layers = [] # variable size for number of hidden layers
-
-# input layer
-input_layer = X
-layers.append(input_layer)
-
-# hidden layers (ReLU activation)
-for i in range(numHiddenLayers):
-    h = tf.layers.dense(layers[-1], hiddenLayers[i], activation=tf.nn.relu,
-        kernel_initializer=initializer)
-    layers.append(h)
+        return
 
 
-# output layer
-output_layer = tf.layers.dense(layers[-1], numLabels, activation=None)
-predicted = tf.nn.sigmoid(output_layer)
-correct_pred = tf.equal(tf.round(predicted), yGold)
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    ####################################
+    def train(
+        self, numEpochs, learning_rate, trainX, trainY, 
+        valX=None, valY=None, val_epochs=None
+    ):
+        """
+        Train NN on features "trainX" an labels "trainY".
 
-# loss
-cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=yGold, logits=output_layer)
-weighted_cross_entropy = tf.nn.weighted_cross_entropy_with_logits(pos_weight = 4,targets=yGold, logits=output_layer)
-cost = tf.reduce_mean(weighted_cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+        "numEpochs" is the number of training epochs.
+        
+        "valX" and "valY" is the validation set.
+
+        "val_epochs" is the number of epochs between each training evaluation 
+        """
+
+        if valX is None or valY is None:
+            valX = trainX; valY = trainY
+        if val_epochs is None:
+            val_epochs = int(numEpochs/100)
+        
+        # track cost
+        acc_history = np.empty(shape=[1],dtype=float)
+        cost_history = np.empty(shape=[1],dtype=float)
+        val_acc_history = np.empty(shape=[1],dtype=float)
+        val_cost_history = np.empty(shape=[1],dtype=float)
+
+        sess = self._sess # tf session
+        sess.run(tf.global_variables_initializer())
+
+        for step in range(numEpochs + 1):
+            # train
+            sess.run(self._optimizer, feed_dict={self._X: trainX, self._yGold: trainY})
+            # compute cost and accuracy on TS
+            loss, acc = sess.run([self._cost, self._accuracy],
+                feed_dict={self._X: trainX, self._yGold: trainY})
+            acc_history = np.append(acc_history, acc)
+            cost_history = np.append(cost_history, loss)
+            # compute cost and accuracy on VS
+            if step % val_epochs == 0:
+                loss, acc = sess.run([self._cost, self._accuracy],
+                    feed_dict={self._X: valX, self._yGold: valY})
+                val_acc_history = np.append(acc_history, acc)
+                val_cost_history = np.append(cost_history, loss)
+                print("TS: Epoch: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(step, cost_history[-1], acc_history[-1]))
+                print("VS: Epoch: {:5}\tLoss: {:.3f}\tAcc: {:.2%}\n".format(step, loss, acc))
+
+        # Final cost and accuracy on VS   
+        loss, acc = sess.run([self._cost, self._accuracy],
+            feed_dict={self._X: valX, self._yGold: valY})
+        val_acc_history = np.append(acc_history, acc)
+        val_cost_history = np.append(cost_history, loss)
+        print("Final VS: Epoch: {:5}\tLoss: {:.3f}\tAcc: {:.2%}\n".format(step, loss, acc))
 
 
-###################################################################################################
-# session
+        return acc_history, cost_history, val_acc_history, val_cost_history
 
-# track cost
-acc_history = np.empty(shape=[1],dtype=float)
-cost_history = np.empty(shape=[1],dtype=float)
 
-with tf.Session() as sess:
 
-    sess.run(tf.global_variables_initializer())
 
-    for step in range(numEpochs):
-        sess.run(optimizer, feed_dict={X: trainX, yGold: trainY})
-        loss, _, acc = sess.run([cost, optimizer, accuracy],
-            feed_dict={X: trainX, yGold: trainY})
-        acc_history = np.append(acc_history, acc)
-        cost_history = np.append(cost_history, loss)
-        if step % int(numEpochs/20) == 0 and printOutput:
-            print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(
-                step, loss, acc))
-            
-    # Test model and check accuracy
-    if printOutput:
-        print('Test Accuracy:', sess.run(accuracy,
-            feed_dict={X: testX, yGold: testY}))
+    ####################################
+    def predict(self, testX, testY):
+
+        predicted, loss, acc = self._sess.run(
+            [self._predicted, self._cost, self._accuracy],
+            feed_dict={self._X: testX, self._yGold: testY}
+        )
+
+        print("Test Set:\tLoss: {:.3f}\tAcc: {:.2%}\n".format(loss, acc))
+
+        return predicted, loss, acc
+
+
+
+
+    ####################################
+    def close_session(self):
+        """
+        Close tf session, release resources
+        """
+
+        self._sess.close()
+        print("Tensorflow session closed.\n")
+
+        return
+
     
-    # Save test results to file
-    test_predicted = sess.run(tf.cast(tf.round(predicted), tf.int32),
-        feed_dict={X: testX})
-    test_predicted_score = sess.run(predicted,
-        feed_dict={X: testX})
-    #print(testY)
-    test_actual = testY
-    #results = pd.DataFrame({'Predicted':test_predicted,'Actual':test_actual})
-    #results.to_csv(test_set_result_path, index=False)
+    '''
+    Error: Key beta1_power_1 not found in checkpoint
 
-###################################################################################################
+    ####################################
+    def save_session(self, save_path_str):
+        """
+        Save tf session to file
+        """
 
-# Confusion Matrix
-if printConfusionMatrix:
-    print(confusion_matrix(test_actual, test_predicted))
+        # Save the variables to disk.
+        save_path = self._saver.save(self._sess, save_path_str)
+        print("Model saved in path: %s\n" % save_path)
+            
+        return
 
-# Plot loss and accuracy
-if plotLoss:
-    plt.subplot(121)
-    plt.plot(range(len(cost_history)),cost_history)
-    plt.title('Cost')
 
-    plt.subplot(122)
-    #plt.plot(range(len(acc_history)),acc_history)
-    plt.title('Accuracy')
-    plt.axis([0,numEpochs,max(acc_history)-0.05,max(acc_history)])
+    
+    ####################################
+    def load_session(self, load_path_str):
+        """
+        Load tf session from file
+        """
 
-    plt.show()
+        # Load variables from disk.
+        self._saver.restore(self._sess, load_path_str)
+        print("Model in path: %s restored." % load_path_str)
+            
+        return
+    '''
 
-# Plot precision/recall
-if plotPRCurve:
-    precision, recall, _ = precision_recall_curve(test_actual, test_predicted_score)
-    average_precision = average_precision_score(test_actual, test_predicted_score)
 
-    # In matplotlib < 1.5, plt.fill_between does not have a 'step' argument
-    step_kwargs = ({'step': 'post'}
-                if 'step' in signature(plt.fill_between).parameters
-                else {})
-    plt.step(recall, precision, color='b', alpha=0.2,
-            where='post')
-    plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
 
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.ylim([0.0, 1.05])
-    plt.xlim([0.0, 1.0])
-    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
-            average_precision))
-    plt.show()
+# Usage example
+if __name__=="__main__":
+
+    import pandas as pd # load csv data
+    import numpy as np
+    from sklearn.model_selection import train_test_split # split into training and test set
+    import dataset as ds # load dataset
+
+    # load dataset
+    noShow = ds.import_data_df([ds._FILE_PATHS['merged']])
+    noShow_X, noShow_y = noShow.iloc[:, :-1].values, noShow.iloc[:, -1].values
+    noShow_y = np.array([[i] for i in noShow_y])
+
+    # separate training and test set randomly keeping classes ratio
+    trainX, testX, trainY, testY = train_test_split(
+        noShow_X, noShow_y, test_size=0.33, random_state=42,
+        stratify = noShow_y
+    )
+
+    # Hidden layers
+    hiddenLayers = [10, 5] 
+    # number of features
+    numFeatures = trainX.shape[1]
+    # number of classes
+    numLabels = trainY.shape[1]
+
+    # init
+    NN = NN_Sigmoid(
+        hiddenLayers, numFeatures, numLabels, 0.01,
+        cross_entropy_weight = 4,
+        optimizer = "Adam"
+    )
+    # train
+    NN.train(
+            150, 0.01, trainX, trainY, 
+            valX=testX, valY=testY, val_epochs=50
+        )
+    # test
+    NN.predict(
+        testX, testY
+    )
+    # save session
+    #NN.save_session("Saved_sessions/model.ckpt")
+    # close tf session
+    NN.close_session()
+
+    '''
+    # create new untrained NN
+    NN2 = NN_Sigmoid(
+        hiddenLayers, numFeatures, numLabels, 0.01,
+        cross_entropy_weight = 4,
+        optimizer = "Adam"
+    )
+    # load session
+    NN2.load_session("Saved_sessions/model.ckpt")
+    # test
+    NN2.predict(
+        testX, testY
+    )
+    '''
---
+++ Code/no_show.py
@@ -0,0 +1,74 @@
+# Hospital appointment no-show prediction
+# Simple NN using TensorFlow
+# April 2019
+# Eduardo Moura Cirilo Rocha
+
+import pandas as pd # load csv data
+import numpy as np
+from sklearn.model_selection import train_test_split # split into training and test set
+
+import dataset as ds # load dataset
+import NN_Sigmoid as nns # implementation of DecisionTree
+
+
+
+noShow = ds.import_data_df([ds._FILE_PATHS['merged']])
+noShow_X, noShow_y = noShow.iloc[:, :-1].values, noShow.iloc[:, -1].values
+noShow_y = np.array([[i] for i in noShow_y])
+#noShow_y= pd.get_dummies(noShow_y).values
+
+# separate training and test set randomly keeping classes ratio
+trainX, testX, trainY, testY = train_test_split(
+    noShow_X, noShow_y, test_size=0.33, random_state=42,
+    stratify = noShow_y
+)
+
+
+
+
+# Hidden layers
+hiddenLayers = [10] 
+# number of features
+numFeatures = trainX.shape[1]
+# number of classes
+numLabels = trainY.shape[1]
+
+
+# init
+NN = nns.NN_Sigmoid(hiddenLayers, numFeatures, numLabels, 0.01,
+    cross_entropy_weight = 4,
+    optimizer = "Adam")
+
+# train
+NN.train(
+        150, 0.01, trainX, trainY, 
+        valX=testX, valY=testY, val_epochs=50
+    )
+
+
+# test
+NN.predict(
+    testX, testY
+)
+
+# save session
+NN.save_session("Saved_sessions/model.ckpt")
+
+
+# close tf session
+NN.close_session()
+
+
+# create new untrained NN
+NN2 = nns.NN_Sigmoid(hiddenLayers, numFeatures, numLabels, 0.01,
+    cross_entropy_weight = 4,
+    optimizer = "Adam")
+
+
+# load session
+NN2.load_session("Saved_sessions/model.ckpt")
+
+# test
+NN2.predict(
+    testX, testY
+)
